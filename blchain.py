548602c6b623c5b07abb179775cc19cdf2301639
faiaz18 <128858980+faiaz18@users.noreply.github.com>
2024-07-29 17:10:20 +0200

    Update blchain.py
    deleted comments

--- blchain.py
+++ blchain.py
@@ -1,23 +1,23 @@
 import datetime  # Importing the datetime module to work with dates and times.
-import hashlib  # Importing the hashlib module to perform secure hash and message digest algorithms.
-from flask import Flask, request, render_template  # Importing Flask framework for web application development, along with request handling and rendering HTML templates.
+import hashlib  
+from flask import Flask, request, render_template  
 
-class Block:  # Defining a class named Block for creating blocks in the blockchain.
-    blockNo = 0  # Initializing the block number.
-    data = None  # Initializing data attribute of the block.
-    next = None  # Initializing next attribute of the block.
-    hash = None  # Initializing hash attribute of the block.
-    nonce = 0  # Initializing nonce attribute of the block.
-    previous_hash = 0*0  # Initializing previous_hash attribute of the block.
-    timestamp = datetime.datetime.now()  # Setting timestamp to the current date and time using datetime module.
+class Block:  
+    blockNo = 0  
+    data = None  
+    next = None  
+    hash = None  
+    nonce = 0  
+    previous_hash = 0*0  
+    timestamp = datetime.datetime.now()  
 
-    def __init__(self, data, name):  # Defining constructor for the Block class.
-        self.data = data  # Assigning data attribute of the block.
-        self.blockName = name  # Assigning blockName attribute of the block.
+    def __init__(self, data, name):  
+        self.data = data  
+        self.blockName = name  
 
-    def hash(self):  # Defining a method to calculate hash of the block.
-        h = hashlib.sha256()  # Creating a SHA-256 hash object.
-        h.update(  # Updating hash object with concatenated data.
+    def hash(self):  
+        h = hashlib.sha256()  
+        h.update(  
             str(self.nonce).encode('utf8') +
             str(self.data).encode('utf8') +
             str(self.previous_hash).encode('utf8') +
@@ -51,22 +51,22 @@ class Blockchain:  # Defining a class named Blockchain to manage the blockchain.
             else:
                 block.nonce += 1  # Incrementing nonce if target is not met.
 
-app = Flask(__name__)  # Creating a Flask application instance. ---------------------------------------------------------------------------------------------------
-blockchain = Blockchain()  # Creating a Blockchain instance.
-my_dict = []  # Initializing a list to store mined blocks.
+app = Flask(__name__)  
+blockchain = Blockchain()  
+my_dict = []  
 
-@app.route('/')  # Route decorator for root URL.-------------------------------------------------------------------------------------------------------------------
-def my_form():  # Function to handle GET requests.-----------------------------------------------------------------------------------------------------------------
-    return render_template('index.html', content="")  # Rendering the HTML template with no content.---------------------------------------------------------------
+@app.route('/')  
+def my_form():  
+    return render_template('index.html', content="")  
 
-@app.route('/', methods=['POST', 'GET'])  # Route decorator for root URL with POST and GET methods.----------------------------------------------------------------
-def my_form_post():  # Function to handle POST requests.-----------------------------------------------------------------------------------------------------------
-    PayeeName = request.form['PayeeName']  # Getting PayeeName from form data.-------------------------------------------------------------------------------------
-    AmountTransfer = request.form['AmountTransfer']  # Getting AmountTransfer from form data.----------------------------------------------------------------------
+@app.route('/', methods=['POST', 'GET'])  
+def my_form_post():  
+    PayeeName = request.form['PayeeName']  
+    AmountTransfer = request.form['AmountTransfer']  
 
-    nm, tx = blockchain.mine(Block(AmountTransfer, PayeeName))  # Mining a new block. nm will be blockName and tx will be data (l.50)------------------------------
-    my_dict.append([nm, tx])  # Appending block name and data to dictionary.---------------------------------------------------------------------------------------
-    return render_template('index.html', name_list=my_dict)  # Rendering HTML template with updated data.----------------------------------------------------------
+    nm, tx = blockchain.mine(Block(AmountTransfer, PayeeName))  
+    my_dict.append([nm, tx])  
+    return render_template('index.html', name_list=my_dict)  
 
-if __name__ == "__main__":  # Checking if the script is being run directly.----------------------------------------------------------------------------------------
-    app.run(debug=True)  # Running the Flask application in debug mode.--------------------------------------------------------------------------------------------
+if __name__ == "__main__":  
+    app.run(debug=True)  
